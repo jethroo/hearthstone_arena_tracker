b2be04e51ec0c49d8283d3c2ebdd252b7048a615
Carsten Wirth <cwirth79@web.de>
2015-03-28 20:47:23 +0100

    extending controller tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,36 +1,34 @@
 require "rails_helper"
 
 describe ApplicationController, type: :controller do
-  subject { ApplicationController.new }
-
   describe "require_login" do
     context "not logged in" do
       let(:response) { double("response") }
 
       before do
-        allow(subject).to receive(:logged_in?).and_return(false)
-        allow(subject).to receive(:redirect_to).with(:root).and_return(response)
-        allow(subject).to receive(:flash).and_return({ alert: ""})
+        allow(controller).to receive(:logged_in?).and_return(false)
+        allow(controller).to receive(:redirect_to).with(:root).and_return(response)
+        allow(controller).to receive(:flash).and_return({ alert: "" })
       end
 
       after do
-        expect(subject).to have_received(:logged_in?)
-        expect(subject).to have_received(:redirect_to).with(:root)
-        expect(subject).to have_received(:flash)
+        expect(controller).to have_received(:logged_in?)
+        expect(controller).to have_received(:redirect_to).with(:root)
+        expect(controller).to have_received(:flash)
       end
 
       it "should redirect_to :root" do
-        expect(subject.require_login).to eq(response)
+        expect(controller.require_login).to eq(response)
       end
     end
 
     context "logged in" do
       before do
-        allow(subject).to receive(:logged_in?).and_return(true)
+        allow(controller).to receive(:logged_in?).and_return(true)
       end
 
       it "is nil" do
-        expect(subject.require_login).to be_nil
+        expect(controller.require_login).to be_nil
       end
     end
   end
--- spec/controllers/arenas_controller_spec.rb
+++ spec/controllers/arenas_controller_spec.rb
@@ -1,5 +1,110 @@
 require "rails_helper"
 
-describe ArenasController, type: :controller do
+RSpec.configure {|c| c.include SessionsHelper }
 
+describe ArenasController, type: :controller do
+  let(:user) { double("user", id: 1) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+
+  describe "new" do
+    before do
+      allow(controller).to receive(:render)
+    end
+
+    after do
+      expect(controller)
+        .to have_received(:render)
+          .with(locals: { arena: instance_of(Arena) })
+    end
+
+    it "will render new arena new" do
+      get :new
+    end
+  end
+
+  describe "create" do
+    let(:params) do
+      { hero: Hero::HEROS.first }
+    end
+
+    context "arena persisted succesfully" do
+      before do
+        allow(controller).to receive(:render)
+      end
+
+      after do
+        expect(controller)
+          .to have_received(:render)
+            .with(:show, locals: { arena: instance_of(Arena) })
+      end
+
+      it "will render arena show" do
+        expect{ post :create, arena: params }
+          .to change{ flash[:success] }
+            .from(nil)
+            .to(/Successfully created new Arena/)
+      end
+    end
+
+    context "arena not persisted" do
+      before do
+        allow(controller).to receive(:render)
+        allow_any_instance_of(Arena).to receive(:persisted?).and_return(false)
+      end
+
+      after do
+        expect(controller)
+          .to have_received(:render)
+            .with(:new, locals: { arena: instance_of(Arena) })
+      end
+
+      it "will render arena new" do
+        expect{ post :create, arena: params }
+          .to change{ flash[:alert] }
+            .from(nil)
+            .to(/Something went wrong/)
+      end
+    end
+  end
+
+  describe "show" do
+    context "if arena found" do
+
+    end
+
+    context "if arena not found" do
+
+    end
+  end
 end
+
+=begin
+
+  def show
+    arena = current_user.arenas.where(id: params[:id]).first
+    if arena
+      render locals: { arena: arena }
+    else
+      redirect_to :root, alert: "Arena not found"
+    end
+  end
+
+  def update
+
+  end
+
+  def index
+    render locals: { arenas: Hash[(1...current_user.arenas.size+1).zip current_user.arenas] }
+  end
+
+  private
+
+  def new_arena_params
+    params.require(:arena).permit(:hero)
+  end
+end
+
+=end
