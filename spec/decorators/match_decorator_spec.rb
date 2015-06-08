2d4282e6bbd1f69bcd136bd99a51135f6ffb464b
Carsten Wirth <cwirth79@web.de>
2015-06-08 07:13:11 +0200

    adding tests for match decorator

--- spec/decorators/match_decorator_spec.rb
+++ spec/decorators/match_decorator_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe MatchDecorator do
   let(:created_at) { double('time') }
 
-  let(:match) do
-    double('match', opponent: 'opponent_test', created_at: created_at)
+  let(:match_double) do
+    double('match', opponent: 'opponent_test', created_at: created_at,
+      hero: 'hero'
+    )
   end
 
-  subject { described_class.new(match) }
+  subject { described_class.new(match_double) }
 
   describe '#opponent' do
     it 'should remove prefix opponent_ for hero' do
@@ -15,7 +17,7 @@ describe MatchDecorator do
     end
   end
 
-  describe 'created_at' do
+  describe '#created_at' do
     before do
       allow(created_at).to receive(:strftime)
     end
@@ -27,6 +29,41 @@ describe MatchDecorator do
 
     it 'formats created_at' do
       subject.created_at
+    end
+  end
+
+  describe '#hero_image' do
+    it 'returns the image path of hero' do
+      expect(subject.hero_image).to eq('/images/heroes/hero_small_circle.png')
+    end
+  end
+
+  describe '#opponent_image' do
+    it 'returns the image path of hero' do
+      expect(subject.opponent_image)
+        .to eq('/images/heroes/test_small_circle.png')
+    end
+  end
+
+  describe '#arena_link' do
+    before do
+      allow(match_double).to receive(:arena).and_return(arena)
+    end
+
+    context 'when no arena' do
+      let(:arena) { nil }
+
+      it 'returns arena path' do
+        expect(subject.arena_link).to eq(nil)
+      end
+    end
+
+    context 'when arena' do
+      let(:arena) { double('arena', id: 1) }
+
+      it 'returns arena path' do
+        expect(subject.arena_link).to match(/arenas/)
+      end
     end
   end
 end
